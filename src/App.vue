43ff508409ad99c1e14bc4746549ed31ffe6cc36
Nekishmyakish <garret613@gmail.com>
2020-10-30 16:39:00 +0300

    appartmentCard added

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="app">
     <Header />
-    <Content ::min="min" :max="max" @update:min="value => min = value" @update:max="value => max = value"></Content>
+    <Content
+      ::min="min"
+      :max="max"
+      @update:min="(value) => (min = value)"
+      @update:max="(value) => (max = value)"
+    ></Content>
+    <AppartmentCard />
   </div>
 </template>
 
 <style>
 #app {
   background-color: #f8f8f8;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Gotham Pro, Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -20,12 +26,14 @@
 <script>
 import Header from "./components/Header";
 import Content from "./components/Content";
+import AppartmentCard from "./components/AppartmentCard";
 
 export default {
   name: "App",
   components: {
     Header,
     Content,
+    AppartmentCard,
   },
 };
 </script>
