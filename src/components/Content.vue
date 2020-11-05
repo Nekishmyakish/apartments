12e879c66fab5491c9a81a7a00f85fc6a9b75512
Федор <arbuzovfp@gmail.com>
2020-11-05 22:35:44 +0300

    fix

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,43 +1,26 @@
 <template>
   <div class="container">
-    <div class="content__top">
-      <AppartmentCardInit class />
-      <AppartmentCardInit />
-      <AppartmentCardInit />
-      <AppartmentCardInit />
-    </div>
-    <div class="content__middle">
-      <AppartmentCardInit />
-      <AppartmentCardInit />
-      <AppartmentCardInit />
-      <AppartmentCardInit />
-    </div>
-    <div class="content__bottom">
-      <AppartmentCardInit />
-      <AppartmentCardInit />
-      <AppartmentCardInit />
-      <AppartmentCardInit />
-    </div>
+    <AppartmentCardInit />
+    <AppartmentCardInit />
+    <AppartmentCardInit />
+    <AppartmentCardInit />
+    <AppartmentCardInit />
+    <AppartmentCardInit />
+    <AppartmentCardInit />
+    <AppartmentCardInit />
+    <AppartmentCardInit />
+    <AppartmentCardInit />
+    <AppartmentCardInit />
+    <AppartmentCardInit />
   </div>
 </template>
 
 <style scoped>
-.content__top,
-.content__middle,
-.content__bottom {
-  margin-left: 50px;
-  margin-right: 50px;
+.container {
   display: flex;
-  align-content: center;
-}
-
-.content__top {
-  margin-top: 50px;
-}
-
-.content__middle {
-  margin-top: 30px;
-  margin-bottom: 30px;
+  flex-wrap: wrap;
+  margin-left: auto;
+  margin-right: auto;
 }
 </style>
 
